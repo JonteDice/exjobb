dd9b50913e61a8a714cac7515add3609f392ffe2
Jonatan <jonatan.eshak@gmail.com>
2024-02-22 11:21:41 +0100

    Removed unnecessary imports and restructured order of imports for better readability

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
 
 from rest_framework import permissions
+# from rest_framework_swagger.views import get_swagger_view
+
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
--- my_site/polls/views.py
+++ my_site/polls/views.py
@@ -4,15 +4,11 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
-
-from .forms import CreatePollForm
-from .models import Poll
-
-
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .forms import CreatePollForm
 from .models import Poll
 from .serializers import CreatePollSerializer
 
